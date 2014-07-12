14952a5196cd1fdb351cc0d7c1cceba351334129
autopp <autopp.inc@gmail.com>
2014-07-13 01:22:19 +0900

    move Varspec::BuiltinMatcher::Or subdir

--- lib/varspec/builtin_matcher.rb
+++ lib/varspec/builtin_matcher.rb
@@ -1,34 +1,5 @@
 module Varspec
   module BuiltinMatcher
-    class Or < Matcher
-      attr_reader :matchers
-      
-      def initialize(*matchers)
-        if matchers.length == 0
-          raise ArgumentError, "require 1 more than matcher"
-        end
-        @matchers = matchers
-      end
-      
-      def invalid_variable?(val)
-        matchers.each do |matcher|
-          if not matcher.invalid_variable?(val)
-            return false
-          end
-        end
-        
-        val.inspect
-      end
-      
-      def to_s
-        if matchers.length == 1
-          matchers[0].inspect
-        else
-          matchers[0, matchers.length-1].map(&:inspect).join(', ') + " or #{matchers[-1]}"
-        end
-      end
-    end
-    
     class Maybe < Or
       def initialize(matcher)
         @matcher = matcher
@@ -135,6 +106,7 @@ require 'varspec/builtin_matcher/boolean'
 require 'varspec/builtin_matcher/eq'
 require 'varspec/builtin_matcher/not'
 require 'varspec/builtin_matcher/and'
+require 'varspec/builtin_matcher/or'
 
 # Define module function corresponding each builtin matcher
 module Varspec
---
+++ lib/varspec/builtin_matcher/or.rb
@@ -0,0 +1,32 @@
+module Varspec
+  module BuiltinMatcher
+    class Or < Matcher
+      attr_reader :matchers
+      
+      def initialize(*matchers)
+        if matchers.length == 0
+          raise ArgumentError, "require 1 more than matcher"
+        end
+        @matchers = matchers
+      end
+      
+      def invalid_variable?(val)
+        matchers.each do |matcher|
+          if not matcher.invalid_variable?(val)
+            return false
+          end
+        end
+        
+        val.inspect
+      end
+      
+      def to_s
+        if matchers.length == 1
+          matchers[0].inspect
+        else
+          matchers[0, matchers.length-1].map(&:inspect).join(', ') + " or #{matchers[-1]}"
+        end
+      end
+    end
+  end
+end
