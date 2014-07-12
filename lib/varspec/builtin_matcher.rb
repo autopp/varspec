9092181c52d1176046e4d539cce4d8b36ca29f54
autopp <autopp.inc@gmail.com>
2014-07-13 00:01:52 +0900

    move builtin matchers subdir

--- lib/varspec/builtin_matcher.rb
+++ lib/varspec/builtin_matcher.rb
@@ -2,33 +2,6 @@ require 'singleton'
 
 module Varspec
   module BuiltinMatcher
-    class ArrayOf < Matcher
-      attr_reader :matcher
-      
-      def initialize(matcher)
-        @matcher = matcher
-      end
-      
-      def invalid_variable?(val)
-        if not val.is_a?(Array)
-          return val.inspect
-        else
-          val.each_with_index do |x, i|
-            if msg = matcher.invalid_variable?(x)
-              return "invalid element (index = #{i}): #{msg}"
-            end
-          end
-        end
-        
-        false
-      end
-      
-      def to_s
-        "[#{matcher}]"
-      end
-      
-    end
-    
     class HashOf < Matcher
       attr_reader :key_matcher, :value_matcher
     
@@ -186,7 +159,100 @@ module Varspec
       end
     end
     
-    # Define module function corresponding each builtin matcher
+    class Truthy < Matcher
+      def initialize
+      end
+      
+      def invalid_variable?(val)
+        if val
+          false
+        else 
+          val.inspect
+        end
+      end
+      
+      def to_s        
+        "truthy value"
+      end
+    end
+    
+    class Falsey < Matcher
+      def initialize
+      end
+      
+      def invalid_variable?(val)
+        if val
+          val.inspect
+        else
+          false
+        end
+      end
+      
+      def to_s
+        "falsey value"
+      end
+    end
+    
+    class Any < Matcher
+      def initialize
+      end
+      
+      def invalid_variable?(val)
+        false
+      end
+      
+      def to_s
+        "anything"
+      end
+    end
+    
+    class None < Matcher
+      def initialize
+      end
+      
+      def invalid_variable?(val)
+        val.inspect
+      end
+      
+      def to_s
+        "nothing"
+      end
+    end
+    
+    class RespondTo < Matcher
+      attr_reader :method_names
+      def initialize(*method_names)
+        if method_names.any?{ |name| !name.is_a?(Symbol) && !name.is_a?(String) }
+          raise TypeError, "require Symbol or String"
+        end
+        
+        @method_names = method_names
+      end
+      
+      def invalid_variable?(val)
+        method_names.each do |name|
+          if !val.respond_to(name)
+            return val.inspect
+          end
+        end
+        
+        false
+      end
+      
+      def to_s
+        "respond_to #{method_names.join}"
+      end
+    end
+    
+  end
+end
+
+# load each builtin matcher
+require 'varspec/builtin_matcher/array_of'
+
+# Define module function corresponding each builtin matcher
+module Varspec
+  module BuiltinMatcher
     self.constants(false).select{ |c| self.const_get(c, false) < Matcher }.each do |c|
       eval <<-EOS
         def #{c}(*args)
---
+++ lib/varspec/builtin_matcher/array_of.rb
@@ -0,0 +1,27 @@
+module Varspec
+  module BuiltinMatcher
+    class ArrayOf < Matcher
+      def initialize(matcher)
+        @matcher = matcher
+      end
+      
+      def invalid_variable?(val)
+        if not val.is_a?(Array)
+          return val.inspect
+        else
+          val.each_with_index do |x, i|
+            if msg = matcher.invalid_variable?(x)
+              return "at index #{i}: #{msg}"
+            end
+          end
+        end
+        
+        false
+      end
+      
+      def to_s
+        "[#{matcher}]"
+      end
+    end
+  end
+end
